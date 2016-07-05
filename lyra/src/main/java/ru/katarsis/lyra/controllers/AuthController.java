6365ecd227adc8da1791d0a76f59403ae31a9bc5
PMKlimov <PMKlimov@PMKLIMOV-PC.gksm.local>
2016-07-05 17:00:34 +0300

    Create cover page

---
+++ lyra/src/main/java/ru/katarsis/lyra/controllers/AuthController.java
@@ -0,0 +1,18 @@
+/**
+ * @author Petr Klimov
+ * 
+ */
+package ru.katarsis.lyra.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AuthController {
+
+    @RequestMapping(value ="/login", method = RequestMethod.GET)
+    public String doLogin(){
+        return "/auth/login";
+    }
+}
--- lyra/src/main/java/ru/katarsis/lyra/controllers/HomeController.java
+++ lyra/src/main/java/ru/katarsis/lyra/controllers/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
     
-    @RequestMapping(value ="/index", method = RequestMethod.GET)
+    @RequestMapping(value ="/", method = RequestMethod.GET)
     public String doHome(){
         return "home";
     }
