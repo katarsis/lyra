3a0f31273607d0252de9498daa9d6d4663cf4891
PMKlimov <PMKlimov@PMKLIMOV-PC.gksm.local>
2016-08-10 10:22:35 +0300

    - some small refactoring

--- lyra/src/main/java/ru/katarsis/lyra/controllers/FileUploadController.java
+++ lyra/src/main/java/ru/katarsis/lyra/controllers/FileUploadController.java
@@ -4,28 +4,18 @@
  */
 package ru.katarsis.lyra.controllers;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
-
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ResourceLoader;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,6 +23,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import ru.katarsis.lyra.service.UserFilesService;
 import ru.katarsis.lyra.service.UserSessionService;
 
 @Controller
@@ -40,51 +31,29 @@ public class FileUploadController {
 
     private static final Logger logger = LoggerFactory.getLogger(FileUploadController.class);
 
-    public static final String ROOT = "D:\\lyra\\upload_dir";
-
-    private final ResourceLoader resourceLoader;
+    @Value("${upload.root}")
+    private String ROOT;
 
     @Autowired
     UserSessionService userSessionService;
     
     @Autowired
-    public FileUploadController(ResourceLoader resourceLoader) {
-        this.resourceLoader = resourceLoader;
+    UserFilesService userFilesService;
+    
+    @Autowired
+    public FileUploadController() {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/dashboard/decision/tree/uploadData")
     public String provideUploadInfo(Model model) throws IOException {
-        model.addAttribute("file", Files.walk(Paths.get(ROOT))
-                .filter(path -> !path.equals(Paths.get(ROOT)))
-                .map(path -> Paths.get(ROOT).relativize(path))
-                .map(path -> linkTo(methodOn(FileUploadController.class).getFile(path.toString())).withRel(path.toString()))
-                .collect(Collectors.toList()));
-        
         return "/dashboard/uploadForm";
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "/{filename:.+}")
-    @ResponseBody
-    public ResponseEntity<?> getFile(@PathVariable String filename) {
-        try {
-            return ResponseEntity.ok(resourceLoader.getResource("file:" + Paths.get(ROOT, filename).toString()));
-        } catch (Exception e) {
-            return ResponseEntity.notFound().build();
-        }
     }
     
     @RequestMapping(method = RequestMethod.GET, value = "/dashboard/decision/tree/files/list")
     @ResponseBody
     public ResponseEntity<?> getFileList(HttpServletResponse response) {
         try {
-            String folderPath = userSessionService.getCurrentUserName();
-            List<String> uploadedFiles = new ArrayList<String>();
-            Path uploadedFilesPath = Paths.get(ROOT,folderPath);
-            uploadedFiles = Files.walk(uploadedFilesPath)
-                    .filter(path -> !path.equals(Paths.get(ROOT,folderPath)))
-                    .map(path -> Paths.get(ROOT).relativize(path))
-                    .map(path -> path.toFile().getName())
-                    .collect(Collectors.toList());
+            List<String> uploadedFiles = userFilesService.getUserUploadedFiles(userSessionService.getCurrentUserName());
             response.setHeader("Content-Disposition","inline");
             return ResponseEntity.ok(uploadedFiles);
         } catch (Exception e) {
@@ -95,23 +64,7 @@ public class FileUploadController {
 
     @RequestMapping(method = RequestMethod.POST, value = "/dashboard/decision/tree/uploadData")
     public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
-        File userFolder = new File(ROOT+File.separator+userSessionService.getCurrentUserName());
-        logger.info("Trying upload file to folder: "+userFolder.getAbsolutePath());
-        if(!userFolder.exists()){
-            userFolder.mkdirs();
-        }
-        if (!file.isEmpty()) {
-            try {
-                Files.copy(file.getInputStream(), Paths.get(userFolder.getCanonicalPath(), file.getOriginalFilename()));
-                redirectAttributes.addFlashAttribute("message","You successfully uploaded " + file.getOriginalFilename() + "!");
-            } catch (IOException|RuntimeException e) {
-                redirectAttributes.addFlashAttribute("message", "Failued to upload " + file.getOriginalFilename() + " => " + e.getMessage());
-                logger.error("Error while trying upload file: ", e);
-            }
-        } else {
-            redirectAttributes.addFlashAttribute("message", "Failed to upload " + file.getOriginalFilename() + " because it was empty");
-        }
-
+        userFilesService.storeUploadedFile(userSessionService.getCurrentUserName(), file);
         return "redirect:/dashboard/decision/tree/";
     }
  
--- lyra/src/main/java/ru/katarsis/lyra/service/UserFilesService.java
+++ lyra/src/main/java/ru/katarsis/lyra/service/UserFilesService.java
@@ -5,17 +5,29 @@
 package ru.katarsis.lyra.service;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 
 @Service
 public class UserFilesService {
-
+    
+    @Value("${upload.root}")
+    private String ROOT;
+    
     private static final Logger logger = LoggerFactory.getLogger(UserFilesService.class);
+    
     
     public String getDataFromFileSource(File sourcefile){
         String result = null;
@@ -23,6 +35,36 @@ public class UserFilesService {
             result = new String(Files.readAllBytes(Paths.get(sourcefile.getCanonicalPath())), "UTF-8");
         }catch (Exception err){
             logger.error("Error while read source file", err);
+        }
+        return result;
+    }
+    
+    public List<String> getUserUploadedFiles(String userName) throws IOException{
+        String folderPath = userName;
+        List<String> uploadedFiles = new ArrayList<String>();
+        Path uploadedFilesPath = Paths.get(ROOT,folderPath);
+        uploadedFiles = Files.walk(uploadedFilesPath)
+                .filter(path -> !path.equals(Paths.get(ROOT,folderPath)))
+                .map(path -> Paths.get(ROOT).relativize(path))
+                .map(path -> path.toFile().getName())
+                .collect(Collectors.toList());
+        return uploadedFiles;
+    }
+    
+    public boolean storeUploadedFile(String userName,MultipartFile file){
+        File userFolder = new File(ROOT+File.separator+userName);
+        boolean result = false;
+        logger.info("Trying upload file to folder: "+userFolder.getAbsolutePath());
+        if(!userFolder.exists()){
+            userFolder.mkdirs();
+        }
+        if (!file.isEmpty()) {
+            try {
+                Files.copy(file.getInputStream(), Paths.get(userFolder.getCanonicalPath(), file.getOriginalFilename()));
+                result = true;
+            } catch (IOException|RuntimeException e) {
+                logger.error("Error while trying upload file: ", e);
+            }
         }
         return result;
     }
